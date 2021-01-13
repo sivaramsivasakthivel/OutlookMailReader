9b66c3c40628a479436521ffe0e373a85225dac7
sivaramsivasakthivel <47791411+sivaramsivasakthivel@users.noreply.github.com>
2021-01-13 17:23:42 +0530

    OutlookFileReader initial commit

---
+++ OutlookMailReader.py
@@ -0,0 +1,24 @@
+import win32com.client
+outlook = win32com.client.Dispatch('outlook.application')
+mapi = outlook.GetNamespace("MAPI")
+inbox = mapi.GetDefaultFolder(6)
+unread=0
+finalMsg = ""
+for  message in inbox.Items:
+    if message.UnRead == True:
+        unread=unread+1 
+        
+print ("Number of unread mails: {} ".format(unread) )       
+for  message in inbox.Items:  
+    if (unread == 1) and (message.UnRead):        
+        msgSubject=message.Subject
+        msgSender=message.Sender
+        finalMsg= "Received a new mail with Subject: {} from {}.Please check.".format(msgSubject,msgSender)
+        print (finalMsg)    
+    
+if unread > 1 :  
+        finalMsg = "Total number of unread mails is {}.Please check".format(unread)
+        print (finalMsg)
+        
+
+    
